8ff2ab6d7c186ca261dc17270b910c77d551a957
KarolBrz <karol130013b@gmail.com>
2023-01-14 18:10:31 +0100

    ProgramTest1

--- NeuralStyleTransfer/MainWindow.xaml.cs
+++ NeuralStyleTransfer/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using OpenCvSharp;
+using OpenCvSharp.Dnn;
+using Window = System.Windows.Window;
 
 namespace NeuralStyleTransfer
 {
@@ -20,9 +23,58 @@ namespace NeuralStyleTransfer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Net _net;
+        private const int Width = 512;
+        private const int Height = 512;
+        private Mat _content;
+        private Mat _style;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            try
+            {
+                _net = CvDnn.ReadNetFromTensorflow("model.pb");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Nie mozna załadowac modelu: " + e.Message);
+            }
+        }
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            _content = Cv2.ImRead("content.jpg");
+            if (_content.Empty())
+            {
+                MessageBox.Show("An error occurred while loading the content image.");
+                return;
+            }
+
+            _style = Cv2.ImRead("style.jpg");
+            if (_style.Empty())
+            {
+                MessageBox.Show("An error occurred while loading the style image.");
+                return;
+            }
+
+            var result = TransferStyle(_content, _style);
+            Cv2.ImWrite("result.jpg", result);
+        }
+
+        private Mat TransferStyle(Mat content, Mat style)
+        {
+            var contentBlob = CvDnn.BlobFromImage(content, 1.0, new OpenCvSharp.Size(Width, Height));
+            var styleBlob = CvDnn.BlobFromImage(style, 1.0, new OpenCvSharp.Size(Width, Height));
+
+            _net.SetInput(contentBlob, "content");
+            _net.SetInput(styleBlob, "style");
+
+            var output = _net.Forward();
+
+            var result = output.GetBlob("generated").MatRef();
+
+            return result;
         }
     }
 }
