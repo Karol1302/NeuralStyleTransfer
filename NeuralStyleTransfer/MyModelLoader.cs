3d05fcad74663b54e90c4807cd0d7b103d609487
KarolBrz <karol130013b@gmail.com>
2023-01-18 23:25:24 +0100

    Test

--- NeuralStyleTransfer/MyModelLoader.cs
+++ NeuralStyleTransfer/MyModelLoader.cs
@@ -7,7 +7,15 @@ namespace NeuralStyleTransfer
     internal class MyModelLoader
     {
         private readonly Net _net;
-        public MyModelLoader(string modelPath) => _net = Net.ReadNet(modelPath);
+        public MyModelLoader(string modelPath, Target target = default, Backend backend = default)
+        {
+            _net = Net.ReadNetFromTensorflow(modelPath);
+            _net.SetPreferableTarget(Target.CPU);
+            _net.SetPreferableBackend(Backend.INFERENCE_ENGINE);
+
+        }
+
+        //public MyModelLoader(string modelPath) => _net = Net.ReadNetFromONNX(modelPath);
 
         public Net GetModel() => _net;
     }
